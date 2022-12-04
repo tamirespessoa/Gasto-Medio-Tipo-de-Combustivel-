41da5d5f194593f9bd70ee8282efbe5d77923601
Tamires Sousa Pessoa <109546094+tamirespessoa@users.noreply.github.com>
2022-12-04 20:50:10 -0300

    Add files via upload

---
+++ segundo-desafio.js
@@ -0,0 +1,20 @@
+const Gasolina = 4.79;
+const Etanol = 3.79;
+const kmPorLitros = 10;
+const distanciaEmKM = 100;
+const tipoCombustivel = 'Gasolina';
+
+const litrosConsumidos = distanciaEmKM / kmPorLitros;
+
+if (tipoCombustivel === 'Etanol') {
+    const valorGasto = litrosConsumidos * Etanol;
+    console.log(valorGasto.toFixed(2));
+
+} else  {
+    const valorGasto = litrosConsumidos * Gasolina;
+    console.log(valorGasto.toFixed(2));
+}
+
+
+
+
